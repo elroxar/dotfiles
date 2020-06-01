3359a594be27b2895e689fdb9db4eaebe76a878d
Theo Thiedig <theo@thiedig.net>
2020-06-01 18:01:41 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+ln -s ${BASEDIR}/vim/ ~/.vim
+
+# zsh
+ln -s ${BASEDIR}/zshrc ~/.zshrc
+
+# git
+ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+
+# X Server
+ln -s ${BASEDIR}/XCompose ~/.XCompose
+
+# system
+ln -s ${BASEDIR}/config/ ~/.config
